187e836df1e75398eebce0643f7972892c668acd
choijin <jin.choi@cern.ch>
2022-10-04 18:37:29 +0900

    temp

--- submitOptimizeHyperparams.sh
+++ submitOptimizeHyperparams.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 python scripts/submitOptimizeHyperparameters.py --signal MHc-70_MA-15 --background TTLL_powheg
-python scripts/submitOptimizeHyperparameters.py --signal MHc-70_MA-40 --background TTLL_powheg
-python scripts/submitOptimizeHyperparameters.py --signal MHc-70_MA-65 --background TTLL_powheg
+#python scripts/submitOptimizeHyperparameters.py --signal MHc-70_MA-40 --background TTLL_powheg
+#python scripts/submitOptimizeHyperparameters.py --signal MHc-70_MA-65 --background TTLL_powheg
 python scripts/submitOptimizeHyperparameters.py --signal MHc-100_MA-60 --background TTLL_powheg
-python scripts/submitOptimizeHyperparameters.py --signal MHc-100_MA-95 --background TTLL_powheg
-python scripts/submitOptimizeHyperparameters.py --signal MHc-130_MA-15 --background TTLL_powheg
-python scripts/submitOptimizeHyperparameters.py --signal MHc-130_MA-55 --background TTLL_powheg
+#python scripts/submitOptimizeHyperparameters.py --signal MHc-100_MA-95 --background TTLL_powheg
+#python scripts/submitOptimizeHyperparameters.py --signal MHc-130_MA-15 --background TTLL_powheg
+#python scripts/submitOptimizeHyperparameters.py --signal MHc-130_MA-55 --background TTLL_powheg
 python scripts/submitOptimizeHyperparameters.py --signal MHc-130_MA-90 --background TTLL_powheg
-python scripts/submitOptimizeHyperparameters.py --signal MHc-130_MA-125 --background TTLL_powheg
-python scripts/submitOptimizeHyperparameters.py --signal MHc-160_MA-85 --background TTLL_powheg
+#python scripts/submitOptimizeHyperparameters.py --signal MHc-130_MA-125 --background TTLL_powheg
+#python scripts/submitOptimizeHyperparameters.py --signal MHc-160_MA-85 --background TTLL_powheg
 python scripts/submitOptimizeHyperparameters.py --signal MHc-160_MA-155 --background TTLL_powheg
--- submitTrainModels.py
+++ submitTrainModels.py
@@ -7,10 +7,11 @@ from itertools import product
 from time import sleep
 from torch import cuda
 
-SIGNALs = ["MHc-70_MA-15", "MHc-70_MA-40", "MHc-70_MA-65",
-           "MHc-100_MA-60", "MHc-100_MA-95",
-           "MHc-130_MA-15", "MHc-130_MA-55", "MHc-130_MA-90", "MHc-130_MA-125",
-           "MHc-160_MA-85", "MHc-160_MA-155"]
+#SIGNALs = ["MHc-70_MA-15", "MHc-70_MA-40", "MHc-70_MA-65",
+#           "MHc-100_MA-60", "MHc-100_MA-95",
+#           "MHc-130_MA-15", "MHc-130_MA-55", "MHc-130_MA-90", "MHc-130_MA-125",
+#           "MHc-160_MA-85", "MHc-160_MA-155"]
+SIGNALs = ["MHc-70_MA-15", "MHc-100_MA-60", "MHc-130_MA-90", "MHc-160_MA-155"]
 BACKGROUNDs = ["TTLL_powheg"]
 
 def makePopulation(sig, bkg):
