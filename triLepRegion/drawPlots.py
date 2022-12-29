d512a7bab3cc206820d3851a050e28c2e481a84d
Jin Choi <choij@Jins-MacBook-Pro.local>
2022-12-29 19:33:27 +0900

    add plotter

--- triLepRegion/drawPlots.py
+++ triLepRegion/drawPlots.py
@@ -4,25 +4,29 @@ import argparse
 from math import pow, sqrt
 from ROOT import kBlack, kYellow, kGray, kRed, kBlue, kGreen, kMagenta, kCyan, kAzure, kViolet
 from ROOT import TFile
+from Plotter import KinematicCanvas, ComparisonCanvas
 
 # Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--era", required=True, type=str, help="era")
 parser.add_argument("--obs", required=True, type=str, help="observable")
 parser.add_argument("--region", required=True, type=str, help="SR / CR name")
+parser.add_argument("--blind", action="store_true", help="blind mode")
 args = parser.parse_args()
 
 WORKDIR = os.environ['WORKDIR']
 
 config = {"xTitle": "m(A)",
           "yTitle": "Events / 5 GeV",
-          "xRange": [0., 200.],
-          "rebin": 2,
-          "logY": True}
+          "xRange": [70., 120.],
+          #"rebin": 5,
+          "logy": False}
 config["era"] = args.era
 
-# Sample list
-DataStream = "DoubleMoun"
+# Sample List
+if "1E2Mu" in args.region: DataStream = "MuonEG"
+elif "3Mu" in args.region: DataStream = "DoubleMuon"
+else: print(f"wrong region {args.region}"); exit(1)
 NonPrompt = ["DYJets", "DYJets10to50_MG", "TTLL_powheg"]
 Conv = ["ZGToLLG", "TTG"]
 VV = ["WZTo3LNu_amcatnlo", "ZZTo4L_powheg"]
@@ -34,27 +38,41 @@ MASSPOINTs = ["MHc-70_MA-15", "MHc-100_MA-60", "MHc-130_MA-90", "MHc-160_MA-155"
 SYSTEMATICs = ["Central"]
 
 # get histograms
-# signals
 SIGs = {}
+BKGs = {}
+colors = {}
+
+# data
+f = TFile.Open(f"{WORKDIR}/ValidParticleNet/{args.era}/ValidParticleNet_{DataStream}.root")
+data = f.Get(f"{args.region}/{args.obs}"); data.SetDirectory(0)
+f.Close()
+
+# signals
 for masspoint in MASSPOINTs:
     fstring =f"{WORKDIR}/ValidParticleNet/{args.era}/ValidParticleNet_TTToHcToWAToMuMu_{masspoint}.root" 
     assert os.path.exists(fstring)
     f = TFile.Open(fstring)
-    h = f.Get(f"{args.region}/{args.obs}"); h.SetDirectory(0)
-    f.Close()
-    SIGs[masspoint] = h.Clone(masspoint); del h
+    try:
+        h = f.Get(f"{args.region}/{args.obs}"); h.SetDirectory(0)
+        f.Close()
+        SIGs[masspoint] = h.Clone(masspoint); del h
+    except Exception as e:
+        print(masspoint, e)
 
 # backgrounds
-BKGs = {}
+histograms = {}
 for sample in MCSamples:
     fstring = f"{WORKDIR}/ValidParticleNet/{args.era}/ValidParticleNet_{sample}.root"
     if not os.path.exists(fstring):
         fstring = f"{WORKDIR}/ValidParticleNet/{args.era}/ValidParticleNet_SkimTree_SS2lOR3l_{sample}.root" 
     assert os.path.exists(fstring)
     f = TFile.Open(fstring)
-    h = f.Get(f"{args.region}/{args.obs}"); h.SetDirectory(0)
-    f.Close()
-    BKGs[sample] = h.Clone(sample); del h
+    try:
+        h = f.Get(f"{args.region}/{args.obs}"); h.SetDirectory(0)
+        f.Close()
+        histograms[sample] = h.Clone(sample); del h
+    except Exception as e:
+        print(sample, e)
 
 # merge backgrounds
 def addHist(name, hist, histDict):
@@ -63,49 +81,54 @@ def addHist(name, hist, histDict):
     else:
         histDict[name].Add(hist)
 
-colors = {}
-colorList = [kRed, kGreen, kGray, kCyan]
-for i, masspoint in enumerate(MASSPOINTs):
-    colors[masspoint] = colorList[i]
-
-histograms = {}
-histograms['rare'] = None
-histograms['conv'] = None
-histograms['ttX'] = None
-histograms['VV'] = None
-histograms['fake'] = None
+BKGs['rare'] = None
+BKGs['conv'] = None
+BKGs['VV'] = None
+BKGs['ttX'] = None
+BKGs['fake'] = None
 
 for sample in NonPrompt:
-    if not sample in BKGs.keys(): continue
-    addHist("fake", BKGs[sample], histograms)
+    if not sample in histograms.keys(): continue
+    addHist("fake", histograms[sample], BKGs)
 for sample in Conv: 
-    if not sample in BKGs.keys(): continue
-    addHist("conv", BKGs[sample], histograms)
+    if not sample in histograms.keys(): continue
+    addHist("conv", histograms[sample], BKGs)
 for sample in VV:
-    if not sample in BKGs.keys(): continue
-    addHist("VV", BKGs[sample], histograms)
+    if not sample in histograms.keys(): continue
+    addHist("VV", histograms[sample], BKGs)
 for sample in ttX:
-    if not sample in BKGs.keys(): continue
-    addHist("ttX", BKGs[sample], histograms)
+    if not sample in histograms.keys(): continue
+    addHist("ttX", histograms[sample], BKGs)
 for sample in Rare:
-    if not sample in BKGs.keys(): continue
-    addHist("rare", BKGs[sample], histograms)
+    if not sample in histograms.keys(): continue
+    addHist("rare", histograms[sample], BKGs)
     
 # colors
-colors['fake'] = kGray
-colors['conv'] = kCyan
-colors['VV'] = kGreen
-colors['ttX'] = kBlue
-colors['rare'] = kRed
+colors['data'] = kBlack
+colors['rare'] = kMagenta
+colors['conv'] = kGreen
+colors['VV'] = kAzure
+colors['ttX'] = kViolet
+colors['fake'] = kGray+100
 
-for name, color in colors.items():
-    print(name, color)
-
+colorList = [kGray, kBlue, kBlack, kRed]
+for i, masspoint in enumerate(MASSPOINTs):
+    colors[masspoint] = colorList[i]
+    
 # draw plots
-from Plotter import KinematicCanvas
-c = KinematicCanvas(config=config)
-c.drawSignals(SIGs, colors)
-c.drawBackgrounds(histograms, colors)
-c.drawLegend()
-c.finalize()
-c.savefig(f"{WORKDIR}/triLepRegion/test.png")
+if args.blind:
+    c = KinematicCanvas(config=config)
+    c.drawSignals(SIGs, colors)
+    c.drawBackgrounds(BKGs, colors)
+    c.drawLegend()
+    c.finalize()
+    c.savefig(f"{WORKDIR}/triLepRegion/test.png")
+else:
+    c = ComparisonCanvas(config=config)
+    c.drawSignals(SIGs, colors)
+    c.drawBackgrounds(BKGs, colors)
+    c.drawData(data)
+    c.drawRatio()
+    c.drawLegend()
+    c.finalize()
+    c.savefig(f"{WORKDIR}/triLepRegion/test.png")
