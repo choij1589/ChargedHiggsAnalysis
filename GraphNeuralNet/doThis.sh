e508e66b85ad6a372176c1a101e63d7b7696067b
Jin Choi <choij@cern.ch>
2023-06-18 15:28:31 +0900

    temp

--- GraphNeuralNet/doThis.sh
+++ GraphNeuralNet/doThis.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background nonprompt
+python evalModels.py --channel Skim3Mu --signal MHc-160_MA-85 --background nonprompt
 python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background ttZ
 #python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background ttZ
