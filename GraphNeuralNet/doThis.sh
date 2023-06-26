5e55ea04fa3e7bb38f5d7120ee7e740b27770a6a
Jin Choi <choij@cern.ch>
2023-06-26 23:18:33 +0900

    training results?

--- GraphNeuralNet/doThis.sh
+++ GraphNeuralNet/doThis.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python evalModels.py --channel Skim3Mu --signal MHc-160_MA-85 --background nonprompt
-python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background ttZ
+python evalModels.py --channel Skim1E2Mu --signal MHc-100_MA-95 --background diboson
+python evalModels.py --channel Skim1E2Mu --signal MHc-130_MA-90 --background diboson
 #python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background ttZ
