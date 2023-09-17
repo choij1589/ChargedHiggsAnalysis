41de9ff3fc19764e19849ecac686a0b489b96fc4
Jin Choi <choij@cern.ch>
2023-09-17 22:04:02 +0900

    ParticleNet training - Skim3Mu (epochs 120)

--- GraphNeuralNet/doThis.sh
+++ GraphNeuralNet/doThis.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-python evalModels.py --channel Skim1E2Mu --signal MHc-70_MA-65 --background nonprompt --epochs 80
-python evalModels.py --channel Skim1E2Mu --signal MHc-130_MA-90 --background nonprompt --epochs 80
-python evalModels.py --channel Skim1E2Mu --signal MHc-160_MA-85 --background nonprompt --epochs 80
+python evalModels.py --channel Skim3Mu --signal MHc-70_MA-65 --background nonprompt --epochs 120
+python evalModels.py --channel Skim3Mu --signal MHc-100_MA-95 --background nonprompt --epochs 120
+python evalModels.py --channel Skim3Mu --signal MHc-130_MA-90 --background nonprompt --epochs 120
+python evalModels.py --channel Skim3Mu --signal MHc-160_MA-85 --background nonprompt --epochs 120
+python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background nonprompt --epochs 120
+python evalModels.py --channel Skim3Mu --signal MHc-70_MA-65 --background diboson --epochs 120
+python evalModels.py --channel Skim3Mu --signal MHc-100_MA-95 --background diboson --epochs 120
+python evalModels.py --channel Skim3Mu --signal MHc-130_MA-90 --background diboson --epochs 120
+python evalModels.py --channel Skim3Mu --signal MHc-160_MA-85 --background diboson --epochs 120
+python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background diboson --epochs 120
+python evalModels.py --channel Skim3Mu --signal MHc-70_MA-65 --background ttZ --epochs 120
+python evalModels.py --channel Skim3Mu --signal MHc-100_MA-95 --background ttZ --epochs 120
+python evalModels.py --channel Skim3Mu --signal MHc-130_MA-90 --background ttZ --epochs 120
+python evalModels.py --channel Skim3Mu --signal MHc-160_MA-85 --background ttZ --epochs 120
+python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background ttZ --epochs 120
+
