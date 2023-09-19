60a412a37e406ab37c2dca7681a06294b35d3237
Jin Choi <choij@cern.ch>
2023-09-19 21:32:18 +0900

    training results

--- GraphNeuralNet/doThis.sh
+++ GraphNeuralNet/doThis.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
-python evalModels.py --channel Skim3Mu --signal MHc-70_MA-65 --background nonprompt --epochs 120
-python evalModels.py --channel Skim3Mu --signal MHc-100_MA-95 --background nonprompt --epochs 120
-python evalModels.py --channel Skim3Mu --signal MHc-130_MA-90 --background nonprompt --epochs 120
-python evalModels.py --channel Skim3Mu --signal MHc-160_MA-85 --background nonprompt --epochs 120
-python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background nonprompt --epochs 120
-python evalModels.py --channel Skim3Mu --signal MHc-70_MA-65 --background diboson --epochs 120
-python evalModels.py --channel Skim3Mu --signal MHc-100_MA-95 --background diboson --epochs 120
-python evalModels.py --channel Skim3Mu --signal MHc-130_MA-90 --background diboson --epochs 120
-python evalModels.py --channel Skim3Mu --signal MHc-160_MA-85 --background diboson --epochs 120
-python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background diboson --epochs 120
-python evalModels.py --channel Skim3Mu --signal MHc-70_MA-65 --background ttZ --epochs 120
-python evalModels.py --channel Skim3Mu --signal MHc-100_MA-95 --background ttZ --epochs 120
-python evalModels.py --channel Skim3Mu --signal MHc-130_MA-90 --background ttZ --epochs 120
-python evalModels.py --channel Skim3Mu --signal MHc-160_MA-85 --background ttZ --epochs 120
-python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background ttZ --epochs 120
+python evalModels.py --channel Skim3Mu --signal MHc-70_MA-65 --background nonprompt --epochs 90
+python evalModels.py --channel Skim3Mu --signal MHc-100_MA-95 --background nonprompt --epochs 90
+python evalModels.py --channel Skim3Mu --signal MHc-160_MA-85 --background nonprompt --epochs 90
+python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background nonprompt --epochs 90
+python evalModels.py --channel Skim3Mu --signal MHc-70_MA-65 --background ttZ --epochs 100
+python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background ttZ --epochs 100
 
