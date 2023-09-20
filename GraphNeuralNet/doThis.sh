40fa7cd4afd4d898365cac146b57c69b0c51b548
Jin Choi <choij@cern.ch>
2023-09-20 13:04:57 +0900

    fakerate measurement - 2018

--- GraphNeuralNet/doThis.sh
+++ GraphNeuralNet/doThis.sh
@@ -3,6 +3,4 @@ python evalModels.py --channel Skim3Mu --signal MHc-70_MA-65 --background nonpro
 python evalModels.py --channel Skim3Mu --signal MHc-100_MA-95 --background nonprompt --epochs 90
 python evalModels.py --channel Skim3Mu --signal MHc-160_MA-85 --background nonprompt --epochs 90
 python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background nonprompt --epochs 90
-python evalModels.py --channel Skim3Mu --signal MHc-70_MA-65 --background ttZ --epochs 100
-python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background ttZ --epochs 100
-
+python evalModels.py --channel Skim3Mu --signal MHc-160_MA-120 --background ttZ --epochs 90
